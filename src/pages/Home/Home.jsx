18d06c78ce29cbd8f757fbc1519f5ff96cfeaa64
Andrew Motko <andrew.motko@gmail.com>
2024-03-02 15:26:44 +0200

    add: MoviesList component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,10 @@
-import { Suspense, useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { MoviesList } from './MoviesList/MoviesList';
 
-import apiWeekMovies from 'components/api/requests';
-import { HomeItem } from './HomeItem/HomeItem';
-
-const Home = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    apiWeekMovies.fetchDataWeek().then(({ results }) => {
-      setMovies(results);
-    });
-  }, []);
-
+export const Home = () => {
   return (
     <main>
       <h1>Trends of the week</h1>
-      <ul>
-        {movies.map(({ title, id }) => (
-          <HomeItem title={title} id={id} key={id} />
-        ))}
-      </ul>
-      <Suspense>
-        <Outlet />
-      </Suspense>
+      <MoviesList />
     </main>
   );
 };
-export default Home;
